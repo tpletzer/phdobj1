55c236d1ac857197e0fb5b3d41e7ca7fb90e15d9
Tamara Pletzer tamara.pletzer@postgrad.otago.ac.nz <pleta922@mahuika01.mahuika.nesi.org.nz>
2021-10-03 08:33:36 +1300

    python scripts for daily q calculations

--- plot_dsclim.py
+++ plot_dsclim.py
@@ -5,20 +5,20 @@ from salem import open_wrf_dataset, get_demo_file
 import xarray as xr
 
 
-def plot_dsclim(wrf_grid = '/Users/tamarapletzer/Desktop/wrfout_d03_2019021900_f015.nc',
-		 ds_clim = '/Users/tamarapletzer/Desktop/ds_clim_monthlyavg_d03201802.nc', masked=True):
+def plot_dsclim(wrf_grid = '/nesi/nobackup/uoo03104/all_files/sfc_wrfout_d03_2019021900_f015.nc',
+		 ds_clim = '/nesi/nobackup/uoo03104/dailyq_summerd03/ds_clim_dailyq_d0320161118.nc', masked=True):
     ds_grid = open_wrf_dataset(wrf_grid)
     ds_data = xr.open_dataset(ds_clim)
 
     ds_data = ds_data.expand_dims('Time')
 
     ds_grid = ds_grid.rename_dims({'time':'Time'})
-    ds_grid = ds_grid.drop_dims('soil_layers')
-    ds_grid = ds_grid.drop_dims('bottom_top')
+    #ds_grid = ds_grid.drop_dims('soil_layers')
+    #ds_grid = ds_grid.drop_dims('bottom_top')
 
-    new = ds_grid.merge(ds_data.monthly_avg_avg_T2d03201802, combine_attrs="override")
+    new = ds_grid.merge(ds_data['q0.950_T2_d0320161118'], combine_attrs="override")
 
-    mask = new['monthly_avg_avg_T2d03201802'].where(new['LANDMASK']==1)
+    mask = new['q0.950_T2_d0320161118'].where(new['LANDMASK']==1)
 
 
     plt.figure()
@@ -31,7 +31,9 @@ def plot_dsclim(wrf_grid = '/Users/tamarapletzer/Desktop/wrfout_d03_2019021900_f
     if masked==True:
          t2 = mask.isel(Time=0)
     else:
-        t2 = new.monthly_avg_avg_T2d03201802.isel(Time=0)
+        t2 = new['q0.950_T2_d0320161118'].isel(Time=0)
 
     t2.plot.pcolormesh(ax=ax, transform=proj)
     plt.show()
+
+plot_dsclim()
