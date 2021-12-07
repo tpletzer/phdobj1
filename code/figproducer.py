23aa33d62e140eedf8e2664a4a9ba08dfeaf13a4
Tamara Pletzer <tamara.pletzer@gmail.com>
2021-12-07 20:34:11 +1300

    plots subdomains

--- code/figproducer.py
+++ code/figproducer.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import cartopy
 import salem
 from salem import open_wrf_dataset, get_demo_file
+import matplotlib.patches as patches
 
 def get_varproj(ds_grid, ds_clim, var):
     ds_data = xr.open_dataset(ds_clim)
@@ -28,6 +29,55 @@ def get_monthyear(filename):
 
     return month, year
 
+def plotsubdom(filename='sfc_wrfout_d03_2018021800_f023.nc', varnmae='HGT'):
+
+    data = xr.open_dataset(filename)
+
+    fig = plt.figure()
+    ax = plt.axes()
+    data[varname].plot()
+    ax.add_patch(patches.Rectangle((0, 871), 239, 116, alpha=0.2, color='red'))
+    ax.add_patch(patches.Rectangle((100, 553), 150, 237, alpha=0.2, color='blue'))
+    ax.add_patch(patches.Rectangle((0, 553), 250, 434, alpha=0.2, color='white'))
+    plt.show()
+
+def oneplot(wrf_grid='sfc_wrfout_d03_2018021800_f023.nc', ds_clim='ds_clim_longtermsumrunoff.nc',
+            masked=True, v='long_sum_SFROFF', t='Sum of surface runoff over seven summers (2013-2020)'):
+         
+     
+    ds_grid = open_wrf_dataset(wrf_grid)
+    ds_data = xr.open_dataset(ds_clim)
+ 
+    ds_data = ds_data.expand_dims('Time')
+
+    ds_grid = ds_grid.rename_dims({'time':'Time'})
+
+
+    new = ds_grid.merge(ds_data[v], combine_attrs="override")
+
+    mask = new.HGT.values.reshape((1035, 675))
+    mask = xr.where(mask>0.0, 1.0, 0.0)
+ 
+ 
+    fig = plt.figure()
+    proj = new.salem.cartopy()
+    ax = plt.axes(projection=proj)
+    ax.coastlines()
+    ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
+    ax.set_extent(new.salem.grid.extent, crs=proj)
+    ax.set_title(t)
+
+    if masked==True:
+        t2 = (new[v].isel(Time=0))*mask
+    else:
+        t2 = new[v].isel(Time=0)
+
+    t2.plot.pcolormesh(ax=ax, transform=proj, vmin=0.0, vmax=1500, cmap=cmocean.cm.ice_r, 
+        cbar_kwargs={"label": "surface runoff (mm)"}, add_labels=False) 
+    plt.show()
+
+    #contourf for contourss instead
+
 def main(*, varname: str, wrfgridfile: str, inputfiles: str, outputfile: str):
     """
     Plotting subplots
@@ -48,7 +98,7 @@ def main(*, varname: str, wrfgridfile: str, inputfiles: str, outputfile: str):
     ncol = 2
     nrow= int(math.ceil(nmonth/ncol))
 
-    #fig, axes = plt.subplots(nrow, ncol)
+
     #fig.suptitle(varname)
     fig = plt.figure(figsize=(8, 7))
     for iplot in range(nmonth):
@@ -59,11 +109,17 @@ def main(*, varname: str, wrfgridfile: str, inputfiles: str, outputfile: str):
                              var=varname)
         proj = ds_new.salem.cartopy()
         ax = plt.subplot(nrow, ncol, iplot + 1, projection=proj)
-        ds_new[varname].isel(Time=0).plot.pcolormesh(ax=ax, 
-                                     transform=proj)
-        # axes[i, j].pcolormesh(ds_new[varname].isel(Time=0)) 
-        # month, year = get_monthyear(files[iplot])
-        #axes[i, j].title(f'{year}-{month}')
+        ax.coastlines()
+        ax.gridlines()
+        #ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
+        #ax.set_extent(ds_new.salem.grid.extent, crs=proj)
+        # ds_new[varname].isel(Time=0).plot.pcolormesh(ax=ax, 
+        #                              transform=proj)
+        ax.set_extent(ds_new.salem.grid.extent, crs=proj)
+        ds_new[varname].isel(Time=0).plot(ax=ax, transform=proj)
+
+        #month, year = get_monthyear(files[iplot])
+        #ax.title(f'{year}-{month}')
 
     plt.show()
 
