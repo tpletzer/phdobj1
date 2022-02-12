3db1a3a758bbcdfca7cdabe5d686da93d9654fce
Tamara Pletzer <tamara.pletzer@gmail.com>
2022-02-13 09:54:56 +1300

    removed bugs

--- code/sfcroff_corr.py
+++ code/sfcroff_corr.py
@@ -9,22 +9,41 @@ def pretime(ds):
     ds = ds.expand_dims('Time')
     return ds
 
+
+
 def addfield_ds(file_avg, file_sfroff):
     ## create a ds merges two fields together that have the same timestep
+
     ds1 = xr.open_dataset(file_avg)
     ds2 = xr.open_dataset(file_sfroff)
     ds1['mo_avgsum_SFROFF'] = (("south_north", "west_east"), ds2['mo_avgsum_SFROFF'].values)
-    mf_ds.to_netcdf('/nesi/nobackup/uoo03104/temp/file_avg')
 
-    return ds1
+    filename = file_avg.split('/')[-1]
+    print(filename)
 
+    ds1.to_netcdf('/nesi/project/uoo03104/amps_monthly/temp/filename')
+
+    return 
+
+
+files_avg = glob.glob('/nesi/project/uoo03104/amps_monthly/monthlyavg_summerd03/*')
+files_avg.sort()
+files_sfroff = glob.glob('/nesi/project/uoo03104/amps_monthly/monthlysumrunoff2_summerd03/*')
+files_sfroff.sort()
+
+for i in range(0, len(files_sfroff)):
+    print(i)
+    addfield_ds(files_avg[i], files_sfroff[i])
+
+
+print("done")
 
 def sfcroff_corr(file_dir, var):
 
     # directory of files
-    files = glob.glob('/nesi/project/uoo03104/temp/*')
+    files = glob.glob('/nesi/project/uoo03104/amps_monthly/temp/*')
     files.sort()
-    
+
     #dataset of the two vars for the same timesteps and sorted (need netcdfs for each time with both fields eg. append to avg)
 
     #land mask and ice shelf mask
