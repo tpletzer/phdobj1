00290bb2f28cf86e5771d6af74c7577b01f32c26
Tamara Pletzer <tamara.pletzer@gmail.com>
2022-02-13 09:42:56 +1300

    preprocessing done

--- code/sfcroff_corr.py
+++ code/sfcroff_corr.py
@@ -5,21 +5,34 @@ import pickle
 import glob
 import xarray as xr
 
-# directory of files
-files = glob.glob('/nesi/project/uoo03104/amps_monthly/monthlysumrunoff2_summerd03/*')
-#dataset of the two vars for the same timesteps and sorted 
+def pretime(ds):
+    ds = ds.expand_dims('Time')
+    return ds
 
-#land mask and ice shelf mask
+def addfield_ds(file_avg, file_sfroff):
+    ## create a ds merges two fields together that have the same timestep
+    ds1 = xr.open_dataset(file_avg)
+    ds2 = xr.open_dataset(file_sfroff)
+    ds1['mo_avgsum_SFROFF'] = (("south_north", "west_east"), ds2['mo_avgsum_SFROFF'].values)
+    mf_ds.to_netcdf('/nesi/nobackup/uoo03104/temp/file_avg')
 
-#compute corr
+    return ds1
 
-#return corr
 
-#plot fn
-files = glob.glob('/nesi/project/uoo03104/amps_monthly/monthlysumrunoff2_summerd03/*')
+def sfcroff_corr(file_dir, var):
 
-files.sort()
+    # directory of files
+    files = glob.glob('/nesi/project/uoo03104/temp/*')
+    files.sort()
+    
+    #dataset of the two vars for the same timesteps and sorted (need netcdfs for each time with both fields eg. append to avg)
 
-ds = xr.open_mfdataset(files, concat_dim='Time', combine='nested')
+    #land mask and ice shelf mask
 
-cor = xr.corr(ds.mo_maxsum_SFROFF, ds.mo_avgsum_SFROFF, dim='Time')
+    #compute corr
+
+    #return corr
+
+    ds = xr.open_mfdataset(files, concat_dim='Time', combine='nested', )
+
+    cor = xr.corr(ds.mo_maxsum_SFROFF, ds.mo_avgsum_SFROFF, dim='Time')
