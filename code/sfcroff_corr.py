756dc0cb1cfb876113b78e9b71e5dde073903c65
Tamara Pletzer <tamara.pletzer@gmail.com>
2022-02-14 10:43:08 +1300

    final run

--- code/sfcroff_corr.py
+++ code/sfcroff_corr.py
@@ -6,28 +6,47 @@ import glob
 import xarray as xr
 
 def pretime(ds): 
+    ds_hgt = xr.open_dataset('/nesi/project/uoo03104/amps_daily/dailyavg_summerd03/ds_clim_dailystats_d0320180205.nc')
+    hgt = ds_hgt.avg_HGTd0320180205
     ds = ds.expand_dims('Time')
     ds['HGT'] = (("south_north", "west_east"), hgt.values)
     return ds
 
 
-def sfcroff_corr(file_dir, var='mo_avg_T2'):
+def sfcroff_corr(file_dir='/nesi/project/uoo03104/amps_monthly/'):
 
     # directory of files
     ds_hgt = xr.open_dataset('/nesi/project/uoo03104/amps_daily/dailyavg_summerd03/ds_clim_dailystats_d0320180205.nc')
     hgt = ds_hgt.avg_HGTd0320180205
 
-    files_avg = glob.glob('/nesi/project/uoo03104/amps_monthly/monthlyavg_summerd03/*')
+    files_avg = glob.glob(file_dir + 'monthlyavg_summerd03/*.nc')
     files_avg.sort()
 
-    files_sfroff = glob.glob('/nesi/project/uoo03104/amps_monthly/monthlysumrunoff2_summerd03/*')
+    files_sfroff = glob.glob(file_dir + 'monthlysumrunoff2_summerd03/*.nc')
     files_sfroff.sort()
 
     ds_avg = xr.open_mfdataset(files_avg, concat_dim='Time', combine='nested', preprocess = pretime)
     ds_sfroff = xr.open_mfdataset(files_sfroff, concat_dim='Time', combine='nested', preprocess = pretime)
 
-    mask_sfroff = ds_sfroff['mo_avgsum_SFROFF'].where(ds_sfroff.HGT>0.0)
-    mask_avg = ds_avg[var].where(ds_sfroff.HGT>0.0)
+    mf_ds = xr.Dataset()
 
-    #compute corr
-    cor = xr.corr(mask_sfroff, mask_avg, dim='Time')
+    for v in ds_avg.var():
+        if v.startswith("mo_avg_"):
+            print(v)
+            mask_sfroff = ds_sfroff['mo_avgsum_SFROFF'].where(ds_sfroff.HGT>0.0)
+            mask_avg = ds_avg[v].where(ds_sfroff.HGT>0.0)
+
+            #compute corr
+            cor = xr.corr(mask_sfroff, mask_avg, dim='Time')
+            mf_ds['cor_' + v] = (("south_north", "west_east"), cor.values)
+        else:
+            continue
+
+
+    mf_ds.to_netcdf(file_dir + 'sfroffcor_' + '.nc')
+    mf_ds.close()
+
+
+sfcroff_corr()
+
+
