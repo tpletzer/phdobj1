4cf6a26ac55ae18af9c8bb63e133c7622053faa3
Tamara Pletzer <tamara.pletzer@gmail.com>
2022-03-03 08:01:13 +1300

    climatology timeseries script

---
+++ code/clim_timeseries.py
@@ -0,0 +1,28 @@
+#script to produce timeseries plot of atmospheric variables from AMPS using mean daily 
+
+import xarray as xr
+import re
+import glob
+import math
+import defopt
+
+def create_df(file_dir):
+    #open files in  df
+
+def time_shift():
+    #convert from forecast time to UTC
+
+def calc_meandaily(var='T2', domain='RSR'):
+    # open files and calculate the mean daily for a given variable and region
+
+
+
+
+def clim_time():
+
+
+
+
+
+
+
